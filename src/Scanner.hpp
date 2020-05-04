b76e76f4e4e17d600f419e1d892ceba9a4afb192
Igor Machado <igor.machado@gmail.com>
2020-05-04 00:21:35 -0300

    cleaned dev version

--- src/Scanner.hpp
+++ src/Scanner.hpp
@@ -1,6 +1,6 @@
 // Scanner Class for C++ - Scanner++
 // https://github.com/optframe/scannerpp
-// version v1.0.1 (development)
+// version v1.x (development)
 // MIT License
 #ifndef SCANNERPP_SCANNER_HPP_
 #define SCANNERPP_SCANNER_HPP_
